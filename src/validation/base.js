15c2836ade14cb927426aadcb932f163fa254885
alex <alex@fpi.cc>
2021-07-19 17:01:39 +0800

    finished the validated with exception

--- src/validation/base.js
+++ src/validation/base.js
@@ -1,24 +1,26 @@
-import { writeErrorLog } from '../middleware/logger'
-import { ObjectSchema } from 'joi';
+import { ObjectSchema } from 'joi'
+import Parameter from '../exception/parameter'
 
 export default class Base {
     set schema (value) {
-        this.defaultSchema = value;
+        this.defaultSchema = value
     }
 
     /**
      * @returns {ObjectSchema}
      */
     get schema () {
-        return this.defaultSchema;
+        return this.defaultSchema
     }
 
     check (data) {
-        const {value, error} = this.schema.validate(data);
+        const { value, error } = this.schema.validate(data)
         if (error) {
-            writeErrorLog(error)
+            throw new Parameter({
+                message: error.message
+            })
         } else {
-            return true;
+            return true
         }
     }
 }
--- src/validation/test.js
+++ src/validation/test.js
@@ -1,16 +1,27 @@
 import Base from './base'
-import Joi from 'joi';
+import Joi from 'joi'
 
 export default class Test extends Base {
     defaultSchema = Joi.object({
-        id: Joi.number().integer().required(),
-        password: Joi.string().min(6).max(16).alphanum(),
-        email: Joi.string().email().required(),
-        repeat_password: Joi.ref('password')
-    }).rule().messages({
-        id: 'id',
-        password: 'Joi.string().min(6).max(16).alphanum()',
-        email: 'Joi.string().email().required()',
-        repeat_password: "Joi.ref('password')"
+        id: Joi.number().integer().required().messages({
+            'number.base': 'Invalid Id!',
+            'number.integer': 'Invalid Id!',
+            'number.required': 'Invalid Id!',
+        }),
+        password: Joi.string().min(6).max(16).alphanum().messages({
+            'string.base': 'password must be 6-16 length!',
+            'string.min': 'password must be 6-16 length!',
+            'string.max': 'password must be 6-16 length!',
+            'string.alphanum': 'password must be 6-16 length!',
+        }),
+        email: Joi.string().email().required().messages({
+            'string.base': 'Invalid email!',
+            'string.email': 'Invalid email!',
+            'string.required': 'Please enter email!',
+        }),
+        repeat_password: Joi.any().valid(Joi.ref('password')).required().messages({
+            'any.required': 'Please enter repeat_password',
+            'any.only': "Must be same as password",
+        })
     })
 }
