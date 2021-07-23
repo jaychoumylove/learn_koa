99367ca2a471f37e17fe72d755d932d8ffd60069
alex <alex@fpi.cc>
2021-07-23 11:51:49 +0800

    mb logger.js

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -5,57 +5,59 @@ import {
 } from 'log4js'
 import path from 'path'
 
+const logConfig = {
+    appenders: {
+        //debug日志
+        debug: {
+            type: 'dateFile',
+            pattern: 'yyyy-MM-dd.log', //通过日期来生成文件
+            alwaysIncludePattern: true, //文件名始终以日期区分
+            encoding: 'utf-8',
+            filename: path.join('logs/debug/'), //生成文件路径和文件名
+        },
+        //warn日志
+        warn: {
+            type: 'dateFile',
+            pattern: 'yyyy-MM-dd.log', //通过日期来生成文件
+            alwaysIncludePattern: true, //文件名始终以日期区分
+            encoding: 'utf-8',
+            filename: path.join('logs/warn/'), //生成文件路径和文件名
+        },
+        //访问日志
+        error: {
+            type: 'dateFile',
+            pattern: 'yyyy-MM-dd.log', //通过日期来生成文件
+            alwaysIncludePattern: true, //文件名始终以日期区分
+            encoding: 'utf-8',
+            filename: path.join('logs/error/'), //生成文件路径和文件名
+        },
+        //系统日志
+        application: {
+            type: 'dateFile',
+            pattern: 'yyyy-MM-dd.log', //通过日期来生成文件
+            alwaysIncludePattern: true, //文件名始终以日期区分
+            encoding: 'utf-8',
+            filename: path.join('logs/application/'), //生成文件路径和文件名
+        },
+        out: {
+            type: 'console',
+        },
+    },
+    categories: {
+        default: { appenders: ['out'], level: levels.ALL },
+        debug: { appenders: ['debug'], level: levels.DEBUG },
+        warn: { appenders: ['warn'], level: levels.WARN },
+        error: { appenders: ['error'], level: levels.ERROR },
+        application: { appenders: ['application'], level: levels.ALL },
+    },
+}
+
 const initLogger = () => {
-    configure({
-        appenders: {
-            //debug日志
-            debug: {
-                type: 'dateFile',
-                pattern: '-yyyy-MM-dd.log', //通过日期来生成文件
-                alwaysIncludePattern: true, //文件名始终以日期区分
-                encoding: 'utf-8',
-                filename: path.join('logs/', 'debug'), //生成文件路径和文件名
-            },
-            //warn日志
-            warn: {
-                type: 'dateFile',
-                pattern: '-yyyy-MM-dd.log', //通过日期来生成文件
-                alwaysIncludePattern: true, //文件名始终以日期区分
-                encoding: 'utf-8',
-                filename: path.join('logs/', 'warn'), //生成文件路径和文件名
-            },
-            //访问日志
-            error: {
-                type: 'dateFile',
-                pattern: '-yyyy-MM-dd.log', //通过日期来生成文件
-                alwaysIncludePattern: true, //文件名始终以日期区分
-                encoding: 'utf-8',
-                filename: path.join('logs/', 'error'), //生成文件路径和文件名
-            },
-            //系统日志
-            application: {
-                type: 'dateFile',
-                pattern: '-yyyy-MM-dd.log', //通过日期来生成文件
-                alwaysIncludePattern: true, //文件名始终以日期区分
-                encoding: 'utf-8',
-                filename: path.join('logs/', 'application'), //生成文件路径和文件名
-            },
-            out: {
-                type: 'console',
-            },
-        },
-        categories: {
-            default: { appenders: ['out'], level: levels.ALL },
-            debug: { appenders: ['debug'], level: levels.DEBUG },
-            warn: { appenders: ['warn'], level: levels.WARN },
-            error: { appenders: ['error'], level: levels.ERROR },
-            application: { appenders: ['application'], level: levels.ALL },
-        },
-    })
+    configure(logConfig)
 }
 
 const getLoggerInterface = (type = 'default') => {
-    const typeDict = ['error', 'application', 'default', 'debug', 'warn']
+    const typeDict = Object.keys(logConfig.categories);
     if (typeDict.indexOf(type) < 0) {
         type = 'default'
     }
@@ -65,9 +67,10 @@ const getLoggerInterface = (type = 'default') => {
 const writeLog = (log, type = 'info') => {
     const typeDict = ['error', 'debug', 'warn']
     if (typeDict.indexOf(type) > -1) {
+        // log with specific types
         getLoggerInterface(type)[type](log)
     }
-    // log all of
+    // log all of types with 'application'
     getLoggerInterface('application')[type](log)
     // output to console
     getLoggerInterface()[type](log)
